27e9b7bd1235b50256bb1426d069320bafe36f8e
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2023-07-17 11:07:28 +0200

    Making PrimitiveDataKernel stand-alone.

--- src/SimplicialMesh/ComputeNearFarDataOps.hpp
+++ src/SimplicialMesh/ComputeNearFarDataOps.hpp
@@ -15,16 +15,15 @@ protected:
             [&,this]( const Int thread )
             {
                 
-                mut<LInt> Av_outer = AvOp.Outer().data();
-                mut< Int> Av_inner = AvOp.Inner().data();
-                mut<Real> Av_value = AvOp.Values().data();
+                mut<LInt> Av_outer   = AvOp.Outer().data();
+                mut< Int> Av_inner   = AvOp.Inner().data();
+                mut<Real> Av_value   = AvOp.Values().data();
     
                 mut<LInt> Diff_outer = DiffOp.Outer().data();
                 mut< Int> Diff_inner = DiffOp.Inner().data();
                 mut<Real> Diff_value = DiffOp.Values().data();
     
-            
-                PrimitiveDataKernel ker ( *this );
+                PrimitiveDataKernel_T ker ( V_coords, simplices, V_charges );
                 
                 const Int simplex_count = simplices.Dimension(0);
                 const Int i_begin = JobPointer<Int>(simplex_count, ThreadCount(), thread  );
@@ -37,7 +36,7 @@ protected:
                     
                     Av_outer[i+1] = (i+1) * SIZE;
                     
-                    ker.s_simplex.Write( &Av_inner[SIZE * i] );
+                    ker.WriteSortedSimplex( &Av_inner[SIZE * i] );
                     fill_buffer<SIZE>( &Av_value[SIZE * i], nth );
                     
                     for( Int k = 0; k < AMB_DIM; ++k )
@@ -48,15 +47,15 @@ protected:
                         
                         Diff_outer[row+1] = rp + SIZE;
                         
-                        ker.s_simplex.Write( &Diff_inner[rp] );
+                        ker.WriteSortedSimplex( &Diff_inner[rp] );
                     }
 
-                    ker.hull.Write   ( P_hull_coords.data(i) );
-                    ker.center.Write ( P_coords.data(i) );
-                    ker.D_f.Write    ( &Diff_value[ HULL_SIZE * i ] );
+                    ker.Hull().Write   ( P_hull_coords.data(i) );
+                    ker.Center().Write ( P_coords.data(i) );
+                    ker.WriteDiffOp    ( &Diff_value[ HULL_SIZE * i ] );
                     
-                    ker.WriteNear    ( P_near.data(i) );
-                    ker.WriteFar     ( P_far.data(i)  );
+                    ker.WriteNear      ( P_near.data(i) );
+                    ker.WriteFar       ( P_far.data(i)  );
                 }
             },
             ThreadCount()
