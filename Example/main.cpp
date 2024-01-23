118b3256ae37303b178cc534d6876a31b4001b38
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2024-01-22 21:28:07 -0500

    Rearranged experiments; added comments.

--- Example/main.cpp
+++ Example/main.cpp
@@ -1,84 +1,83 @@
 #include <iostream>
 
-#include <sys/types.h>
-#include <pwd.h>
+/// This is a simple program meant to make you familiar with how to
+/// - generate a simplicial mesh from data _in memory_;
+/// - compute the tangent-point energy (and derivative) of such a mesh;
+/// - perform matrix-vector operations with the tangent-point metric;
+/// - compute the gradient of the tangent-point energy w.r.t. that metric; and
+/// - find a collision-free step size for a displacement field along the manifold.
+
+/// Moreover, it also shows you how to
+/// - load a an obstacle mesh into a simplicial mesh and
+/// - compute the tangent-point obstacle energy (and its derivative).
 
 
 
-//#define REMESHER_VERBATIM
-
-#define NDEBUG
-#define TOOLS_ENABLE_PROFILER // enable profiler
-#define TOOLS_DEBUG 
+/// Enable the shipped profiler.
+#define TOOLS_ENABLE_PROFILER
 
 #define LAPACK_DISABLE_NAN_CHECK
 
-/// Use these while on a mac. Don't forget to issue the
-/// compiler flag `-framework Accelerate`.
-#define ACCELERATE_NEW_LAPACK
-#include <Accelerate/Accelerate.h>
-
+#ifdef __APPLE__
+/// Use these while on a Mac. Don't forget to send the compiler flag `-framework Accelerate`.
+///
+    #define ACCELERATE_NEW_LAPACK
+    #include <Accelerate/Accelerate.h>
+#else
 /// Use these instead of Accelerate under Windows or Linux, e.g. together with OpenBLAS, Intel oneMKL, AMD AOCL-BLAS. Of course, your path variables or compiler flags should hint the compiler to these files. And you also have to link the according libraries.
-//#include <cblas.h>
-//#include <lapack.h>
+
+/// This should work for OpenBLAS.
+
+    #include <cblas.h>
+    #include <lapack.h>
+
+/// Use this with Intel oneMKL. Please check their documentation if you are unsure.
+
+    //#include <mkl_cblas.h>
+    //#include <mkl_lapack.h>
+
+#endif
 
 #include "../Repulsor.hpp"
-#include "../submodules/Tensors/Sparse.hpp"
 
-using namespace Repulsor;
-using namespace Tensors;
 using namespace Tools;
 
-
-/// We have to toggle which domain dimensions and ambient dimensions shall be supported by runtime polymorphism before we load Repulsor.hpp
-/// You can activate everything you want, but compile times might increase substatially.
-using Int     = Int32;
-using LInt    = Int64;
-using ExtInt  = Int64;
-
-using Real    = Real64;
-using SReal   = Real64;
-using ExtReal = Real64;
+using Int     = int;
+using LInt    = std::size_t;
+using Real    = double;
 
 
-int main(int argc, const char * argv[])
+int main(void)
 {
     print("");
     print("###############################################################");
-    print("###       Test program for Repulsor::SimplicialMesh        ####");
+    print("###      Example program for Repulsor::SimplicialMesh      ####");
     print("###############################################################");
     print("");
     
-    
-    const char * homedir = getenv("HOME");
+    /// Set up profiler to write to `~/Tools_Profile.tsv` and `~/Tools_Log.txt`
+    Profiler::Clear( getenv("HOME") );
 
-    if( homedir == nullptr)
-    {
-        homedir = getpwuid(getuid())->pw_dir;
-    }
-    std::string path ( homedir );
-    
-    Profiler::Clear( path );
-
-    
-//    int thread_count = 8;
     int thread_count = 1;
     
-#include "Meshes.hpp"
     
-    /// Meaning of template parameters:
+    /// Setting up the scalar types to be used by the meshes:
+    using Mesh_T = Repulsor::SimplicialMeshBase<Real,Int,LInt>;
     
-    /// `SimplicialMeshBase<Real, Int, SReal, ExtReal>`;
+    /// Meaning of template parameters of `SimplicialMeshBase<Real,Int,LInt>`:
     ///
-    /// `Real`  = floating point type used for computations regarding energy and metric
-    /// `Int`   = signed integer type used internally, in particular as indices for sparse arrays.
-    ///           `Int = int`       corresponds to MKL's  LP64 mode
-    ///           `Int = long long` corresponds to MKL's ILP64 mode
-    /// `SReal` = ("short real") floating point type for storage of clustering information. Typically we want to use `SReal = double.`
-    /// `ExtReal` = ("external real") floating point type that is used by the outer world, e.g. for submitting the mesh's vertex coordinates and vectors to multiply against the metric.
-    
-    using Mesh_T         = SimplicialMeshBase<Real,Int,LInt,SReal,ExtReal>;
-    
+    /// `Real`      Floating point type used for computations regarding energy and
+    ///             metric. Typically, you want to use `Real = double`.
+    ///
+    /// `Int`       Signed integer type used internally, in particular as indices
+    ///             for sparse arrays.
+    ///             `Int = int`       corresponds to MKL's  LP64 mode
+    ///             `Int = long long` corresponds to MKL's ILP64 mode
+    ///             I recommend using `Int = int` when you can. This requires a bit less memory for the sparse matrices and this speeds up matrix-vector multiplication.
+    ///
+    /// `LInt`      Integer type used for the row pointers of a sparse array.
+    ///             I recommend using `LInt = std::size_t`.
+
     /// `SimplicialMesh_Factory<Mesh_T,n_min,n_max,m_min,m_max>`
     /// is a factory that can create meshes with base type `Mesh_T` and domain dimension from `n_min` to `n_max` and with ambient dimension from `m_min` to `m_max`.
     /// The main purpose of this factory is to tell the compiler which template instantiations of `SimplicialMesh` it shall generate. So this factory is basically about saving compile time.
@@ -87,104 +86,102 @@ int main(int argc, const char * argv[])
     /// Note that with obstacles you might want to use meshes of various dimensions,
     /// so you have to use an according factory.
 
-    using Mesh_Factory_T = SimplicialMesh_Factory<Mesh_T,2,2,3,3>;
     
-    using Energy_T       = EnergyBase<Mesh_T>;
-    using Metric_T       = MetricBase<Mesh_T>;
     
-    /// Create a factory that can make instances of SimplicialMesh with domain dimension in the range 2,...,2 and ambient dimension in the range 3,...,3.
+    /// Instantiate factory that can create instances of `SimplicialMesh<2,3,Real,Int,LInt>`.
+    
+    Repulsor::SimplicialMesh_Factory<Mesh_T,2,2,3,3> mesh_factory;
+    
+    /// We need also base types for the energies and the metrics.
+    using Energy_T = Repulsor::EnergyBase<Mesh_T>;
+    using Metric_T = Repulsor::MetricBase<Mesh_T>;
+    
+    /// Instantiate factories for multipole-accelerated energy and metric, resp.
+    Repulsor::TangentPointEnergy0_Factory<Mesh_T,2,2,3,3> TPE_factory;
+    Repulsor::TangentPointMetric0_Factory<Mesh_T,2,2,3,3> TPM_factory;
 
-    Mesh_Factory_T mesh_factory;
 
-    /// Initialize mesh by the mesh_factory to allow runtime polymorphism.
-    tic("Initializing mesh");
+#include "../Meshes/TwoSpheres.hpp"
+    
+    /// `vertex_coordinates`, `vertex_count`, `simplices`, `simplex_count`, `amb_dim`, and `dom_dim` have been defined in `TwoSpheres.hpp`.
+    
+    tic("Initializing mesh from memory");
+    
+    /// Use`mesh_factory` to initialize a mesh from arrays that are already in memory.
     std::unique_ptr<Mesh_T> M_ptr = mesh_factory.Make(
         &vertex_coordinates[0][0],  vertex_count, amb_dim,   false,
         &simplices[0][0],          simplex_count, dom_dim+1, false,
         thread_count
     );
+    /// I don't like pointers. Give me a reference.
+    auto & M = *M_ptr;
 
-    auto & M = *M_ptr;  // I don't like pointers. Give me a reference.
-
+    toc("Initializing mesh from memory");
+    
+    
+    dump(M.VertexCount());
+    dump(M.SimplexCount());
     dump(M.ThreadCount());
 
-    toc("Initializing mesh");
-
-    /// Some quite decent settings for 2-dimensional surfaces.
+    /// Setup for bounding volume hierarchy and block cluster tree.
+    /// These are the default values, so you will typically not have to bother about these.
+    
+    /// How many simplices to allow at most in the leave nodes of the bounding volume hierarchy.
     M.cluster_tree_settings.split_threshold                        =  2;
     
-    /// Take as many threads as there are used by SimplicialMesh M
-    M.cluster_tree_settings.thread_count                           =  0;
-    
-    /// Set the separation parameters.
+    /// Set the separation parameter for the multipole acceptance criterion.
     M.block_cluster_tree_settings.far_field_separation_parameter   =  0.25;
+    
+    /// Set the separation parameter for the multipole acceptance criterion in adaptive refinement.
     M.adaptivity_settings.theta                                    = 10.0;
 
-    tic("Creating ClusterTree");
-    /// It is not necessary to call this separately.
-    /// It will automatically be called by all routines that require it.
-    M.GetClusterTree();
-    toc("Creating ClusterTree");
-
-    tic("Creating BlockClusterTree");
-    /// It is not necessary to call this separately.
-    /// It will automatically be called by all routines that require it.
-    M.GetBlockClusterTree();
-    toc("Creating BlockClusterTree");
-
-    valprint("M.GetClusterTree().ThreadCount()",M.GetClusterTree().ThreadCount());
-
-    valprint("M.GetClusterTree().NearDim()",M.GetClusterTree().NearDim());
-
-    valprint("number of detected intersections",M.GetBlockClusterTree().PrimitiveIntersectionCount());
-
     print("");
 
+    
+    /// These turned out to be good choices for the parameters of the tangent-point energy for 2-dimensional surfaces.
     const Real q = 6;
     const Real p = 12;
-
-    /// Again some factories.
-    TangentPointEnergy_Factory<Mesh_T,2,2,3,3> TPE_factory;
-    TangentPointMetric_Factory<Mesh_T,2,2,3,3> TPM_factory;
-
+    
     std::unique_ptr<Energy_T> tpe_ptr = TPE_factory.Make( dom_dim, amb_dim, q, p );
-
-    const auto & tpe = *tpe_ptr;
-
     std::unique_ptr<Metric_T> tpm_ptr = TPM_factory.Make( dom_dim, amb_dim, q, p );
 
-    const auto & tpm = *tpm_ptr;
+    /// Get references.
+    const auto & tpe  = *tpe_ptr;
+    const auto & tpm  = *tpm_ptr;
 
-    double en;
-    /// `Mesh_T::CotangentVector_T` is `Tensor2<Real,Int>` in this case. It is a simple container class for heap-allocated matrices.
-    Mesh_T::CotangentVector_T diff;
+    Real en;
 
-    tic("tpe.Energy(M)");
+    tic("Compute tangent-point energy");
     en = tpe.Value(M);
-    toc("tpe.Energy(M)");
+    toc("Compute tangent-point energy");
 
     dump(en);
 
-    tic("tpe.Differential(M)");
+    
+    /// Require differential of tangent-point energy and load it into the variable `diff`.
+    /// Here `Mesh_T::CotangentVector_T` is a simple container class for heap-allocated matrices. (It is a  `Tensors::Tensor2<Real,Int>`.)
+    Mesh_T::CotangentVector_T diff;
+    tic("Compute differential of energy");
     diff = tpe.Differential(M);
-    toc("tpe.Differential(M)");
-
+    toc("Compute differential of energy");
+    
+    /// Alternatively, you can write into any pointer-array like `diff.data()`.
+    
+    tic("Compute differential of energy (pointer version)");
+    tpe.Differential(M,diff.data());
+    toc("Compute differential of energy (pointer version)");
+    
+    tic("Do both at once");
+    en = tpe.Differential(M,diff.data());
+    toc("Do both at once");
+    
     print("");
 
-    TangentPointEnergy_AllPairs_Factory<Mesh_T,2,2,3,3> TPE_AllPairs_factory;
+    /// Let's do something with the metric.
 
-    std::unique_ptr<Energy_T> tpe_slow_ptr = TPE_AllPairs_factory.Make( dom_dim, amb_dim, q, p );
-
-    const auto & tpe_slow = *tpe_slow_ptr;
-
-    tic("tpe_slow.Energy(M)");
-    en = tpe_slow.Value(M);
-    toc("tpe_slow.Energy(M)");
-
-    dump(en);
-    print("");
-
-    /// Mesh_T::TangentVector_T and Mesh_T::CotangentVector_T are both Tensor2<Real,Int> in this example.
+    /// First create a tangent and a cotangent vector.
+    /// `Mesh_T::TangentVector_T` and `Mesh_T::CotangentVector_T` are both equal
+    /// to `Tensor2<Real,Int>` in this example.
     Mesh_T::TangentVector_T   X ( M.VertexCount(), M.AmbDim() );
     Mesh_T::CotangentVector_T Y ( M.VertexCount(), M.AmbDim() );
 
@@ -193,64 +190,122 @@ int main(int argc, const char * argv[])
 
     const Real alpha = 1.0;
     const Real beta  = 0.0;
+
+    tic("Generalized multiplication with metric");
+    /// Compute `Y = alpha * A * x + beta * Y`, where `A` is the Gram matrix if the metric.
+    tpm.MultiplyMetric(M, alpha, X, beta, Y);
+    toc("Generalized multiplication with metric");
+
+    /// This will get substantially faster for the second time because `A` is precomputed now.
+    tic("Generalized multiplication with metric");
+    tpm.MultiplyMetric(M, alpha, X, beta, Y);
+    toc("Generalized multiplication with metric");
     
-    ///Performs generalized matrix-matrix product Y = alpha * A * X + beta * Y, where A is the tangent-point metric.
-
-    tic("tpm.MetricValues(M)");
-    /// It is not necessary to call this separately.
-    /// It will automatically be called by all routines that require it.
-    tpm.MetricValues(M);
-    toc("tpm.MetricValues(M)");
-
-    tic("tpm.MultiplyMetric");
-    /// Compute Y = alpha * A * x + beta * Y.
+    tic("Generalized multiplication with metric");
     tpm.MultiplyMetric(M, alpha, X, beta, Y);
-    toc("tpm.MultiplyMetric");
-
-    tic("tpm.MultiplyMetric");
-    tpm.MultiplyMetric(M, alpha, X, beta, Y);
-    toc("tpm.MultiplyMetric");
-
-    dump(X.MaxNorm());
-    dump(Y.MaxNorm());
+    toc("Generalized multiplication with metric");
 
     print("");
 
+    /// This is how you can get the gradient of the tangent-point energy:
+    const Int  max_iter  = 100;
+    const Real relative_tolerance = 0.00001;
+    
+    Mesh_T::TangentVector_T   gradient ( M.VertexCount(), M.AmbDim() );
+    
+    tic("Solving for gradient");
+    /// We have to solve M.AmbDim() equations at once.
+    tpm.Solve(M, diff.data(), gradient.data(), M.AmbDim(), max_iter, relative_tolerance );
+    toc("Solving for gradient");
+    
+    dump(tpm.CG_IterationCount());
+    dump(tpm.CG_RelativeResiduals());
+    
+    
+    Mesh_T::TangentVector_T downward_gradient = gradient;
+    
+    downward_gradient*= -1.;
+    
+    print("");
+    
+    /// Given a displacement field like `downward_gradient` along the manifold `M`, you can use `MaximumSafeStepSize` to compute an (almost) maximal collision-free step size in the interval [0,1].
+    tic("MaximumSafeStepSize");
+    const Real t = M.MaximumSafeStepSize(downward_gradient.data(), 1.);
+    
+    toc("MaximumSafeStepSize");
+    
+    valprint("Collision-free step size",t);
+    
+    
+    print("");
+    
+    tic("SemiStaticUpdate");
+    /// X = M.VertexCoordinates() + t * downward_gradient.
+    
+    X = downward_gradient;
+    X *= t;
+    X += M.VertexCoordinates();
 
-    /// Initialize mesh by the factory to allow runtime polymorphism.
-    tic("Initialize obstacle mesh");
-    std::unique_ptr<Mesh_T> Q_ptr = mesh_factory.Make(
-        &obstacle_vertex_coordinates[0][0], obstacle_vertex_count,  amb_dim,   false,
-        &obstacle_simplices[0][0],          obstacle_simplex_count, dom_dim+1, false,
-        thread_count
-    );
-    toc("Initialize obstacle mesh");
+    
+    /// Update mesh without rebuilding the bounding volume hierarchy.
+    /// Use this for line search!
+    M.SemiStaticUpdate( X.data() );
+    
+    toc("SemiStaticUpdate");
+    
+    print("");
+    
+    /// Let's see whether the energy has decreased...
+    
+    Real en_new;
+
+    tic("Compute tangent-point energy of updated mesh");
+    en_new = tpe.Value(M);
+    toc("Compute tangent-point energy of updated mesh");
+
+    dump(en_new);
+    
+    dump(en_new < en);
+    
+    print("");
     print("");
 
-    /// Load obstacle into mesh.
+    /// Initialize obstacle mesh and load it into M.
+    ///
+    /// `obstacle_vertex_coordinates`, `obstacle_simplices`, `obstacle_vertex_count`, `obstacle_simplex_count` have been defined in the file `TwoSpheres.hpp`.
+    ///
     tic("Load obstacle");
-    M.LoadObstacle( std::move(Q_ptr) );
+    M.LoadObstacle( 
+        mesh_factory.Make(
+            &obstacle_vertex_coordinates[0][0], obstacle_vertex_count,  amb_dim,   false,
+            &obstacle_simplices[0][0],          obstacle_simplex_count, dom_dim+1, false,
+            thread_count
+        )
+    );
     toc("Load obstacle");
+    
     print("");
 
-    tic("Create obstacle trees");
-    /// It is not necessary to call this separately.
-    /// It will automatically be called by all routines that require it.
-    M.GetObstacleBlockClusterTree();
-    toc("Create obstacle trees");
+    dump(M.GetObstacle().VertexCount());
+    dump(M.GetObstacle().SimplexCount());
+    
     print("");
-
+    
     /// Compute tangent-point energy between mesh and obstacle.
-    TangentPointObstacleEnergy_Factory<Mesh_T,2,2,2,2,3,3> TPOE_factory;
+    
+    /// Instantiate a factory that for creating instacle energies between
+    /// two instances of `SimplicialMesh<2,3,Real,Int,LInt>`.
+    Repulsor::TangentPointObstacleEnergy_Factory<Mesh_T,2,2,2,2,3,3> TPOE_factory;
 
     std::unique_ptr<Energy_T> tpo_ptr = TPOE_factory.Make( dom_dim, dom_dim, amb_dim, q, p );
-
     const auto & tpo = *tpo_ptr;
 
     tic("Compute tangent-point energy between mesh and obstacle");
     en = tpo.Value(M);
     toc("Compute tangent-point energy between mesh and obstacle");
+    
     dump(en);
+    
     print("");
 
     ///Compute derivative also of this energy.
@@ -260,66 +315,6 @@ int main(int argc, const char * argv[])
 
     print("");
     print("");
-
-
-    TangentPointEnergy0_Factory<Mesh_T,2,2,3,3> TPE0_factory;
-    TangentPointMetric0_Factory<Mesh_T,2,2,3,3> TPM0_factory;
-    PseudoLaplacian_Factory    <Mesh_T,2,2,3,3> Prec_factory;
-
-    const Real s = (p - 2)/q;
-
-    dump(s);
-    dump(2-s);
-
-    std::unique_ptr<Energy_T> tpe0_ptr = TPE0_factory.Make( dom_dim, amb_dim, q, p );
-    std::unique_ptr<Metric_T> tpm0_ptr = TPM0_factory.Make( dom_dim, amb_dim, q, p );
-    std::unique_ptr<Metric_T> prec_ptr = Prec_factory.Make( dom_dim, amb_dim, 2-s  );
-
-    const auto & tpe0 = *tpe0_ptr;
-    const auto & tpm0 = *tpm0_ptr;
-    const auto & prec = *prec_ptr;
-
-    dump(tpe0.Value(M));
-
-    tic("tpm0.MetricValues(M)");
-    tpm0.MetricValues(M);
-    toc("tpm0.MetricValues(M)");
-
-    tic("tpm0.MultiplyMetric");
-    tpm0.MultiplyMetric(M, alpha, X, beta, Y);
-    toc("tpm0.MultiplyMetric");
-
-    tic("tpm0.MultiplyMetric");
-    tpm0.MultiplyMetric(M, alpha, X, beta, Y);
-    toc("tpm0.MultiplyMetric");
-
-    dump(X.MaxNorm());
-    dump(Y.MaxNorm());
-
-    tic("prec.MetricValues(M)");
-    prec.MetricValues(M);
-    toc("prec.MetricValues(M)");
-
-    tic("prec.MultiplyMetric");
-    prec.MultiplyMetric(M, alpha, X, beta, Y);
-    toc("prec.MultiplyMetric");
-
-    tic("prec.MultiplyMetric");
-    prec.MultiplyMetric(M, alpha, X, beta, Y);
-    toc("prec.MultiplyMetric");
-
-    dump(X.MaxNorm());
-    dump(Y.MaxNorm());
-    
-    
-    print("");
-    print("");
-
-    Tensor1<Real,Int> b ( M.VertexCount(), 1 );
-    Tensor1<Real,Int> x ( M.VertexCount(), 0 );
-
-    M.H1Solver().Solve( b.data(), x.data() );
-    
     
     return 0;
 }
